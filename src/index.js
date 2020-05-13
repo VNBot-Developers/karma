9036c569497b9703e74dd01a2c9834c1ad71e399
Cuong Tran <clonebmn2itt@gmail.com>
2020-05-13 19:03:06 +0700

    feat: Connect redis and mongo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const { resolve } = require("path");
+const mongoose = require('mongoose');
+const login = require("./login");
+const fs = require('fs');
+const Handler = require('./events');
+const redis = require('redis');
 require('dotenv').config({
     path: resolve(__dirname, '../', '.env')
 });
 
 
-const login = require("./login");
-const fs = require('fs');
-const Handler = require('./events');
 
 const { email, password, pathAppState } = require("./config");
 let appState;
@@ -16,7 +18,31 @@ try {
     fs.writeFileSync(pathAppState, JSON.stringify({}));
 }
 
+mongoose.connect(process.env.MONGODB_STRING, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
+mongoose.connection
+    .once('open', function () {
+        console.log("Connection database success!")
+    })
+    .on('error', function (error) {
+        console.log(error.stack);
+        process.exit(1);
+    });
+
+const cacheClient = redis.createClient({
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || '6379',
+    password: process.env.REDIS_PASSWORD || null,
+
+});
+cacheClient.on("error", function (error) {
+    console.log(error.stack);
+});
 login(email, password, appState, function (error, api) {
     if (error) return console.log(error);
     const handler = new Handler(api);
